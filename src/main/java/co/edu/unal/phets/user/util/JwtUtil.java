d7abb3fbc2d70af9cfe35168a24856fea027bd24
Julian Salomon <jesalomont@unal.edu.co>
2019-10-19 21:56:10 -0500

    * Changed token validity to 24 hours

--- src/main/java/co/edu/unal/phets/user/util/JwtUtil.java
+++ src/main/java/co/edu/unal/phets/user/util/JwtUtil.java
@@ -14,7 +14,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Component
 public class JwtUtil {
 
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 24 * 60 * 60;
     @Value("${jwt.secret}")
     private String secret;
 
