6556e695ff3353aeff667acb51daeb14fdc4d513
Julian Salomon <jesalomont@unal.edu.co>
2019-10-19 21:08:20 -0500

    + Default country values

--- src/main/java/co/edu/unal/phets/user/UserApplication.java
+++ src/main/java/co/edu/unal/phets/user/UserApplication.java
@@ -1,7 +1,13 @@
 package co.edu.unal.phets.user;
 
+import co.edu.unal.phets.user.model.Country;
+import co.edu.unal.phets.user.service.CountryService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -16,4 +22,13 @@ public class UserApplication {
         SpringApplication.run(UserApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner init(CountryService countryService) {
+        List<Country> countries = new ArrayList<>();
+        countries.add(new Country(1L, "Colombia"));
+        countries.add(new Country(2L, "Ecuador"));
+        countries.add(new Country(3L, "México"));
+        countryService.saveCountries(countries);
+        return null;
+    }
 }
